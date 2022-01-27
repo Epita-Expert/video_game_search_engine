b668d4cb974a8f768003bfeea9156610cfb51a53
Minaro <paturetromain@gmail.com>
2022-01-27 22:35:41 +0100

    Add file injector

--- file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
+++ file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
@@ -1,8 +1,18 @@
 package fr.lernejo.fileinjector;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.lernejo.search.api.Game;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class Launcher {
@@ -11,6 +21,24 @@ public class Launcher {
         try (AbstractApplicationContext springContext = new AnnotationConfigApplicationContext(Launcher.class)) {
 
             System.out.println("Hello after starting Spring");
+            if (args.length > 1) throw new RuntimeException("Wrong number of argument");
+
+            File file = Paths.get(args[0]).toFile();
+            if (!file.exists()) throw new FileNotFoundException();
+
+            Game[] games = new ObjectMapper().readValue(file, Game[].class);
+
+            RabbitTemplate template = springContext.getBean(RabbitTemplate.class);
+
+            for (Game game : games) {
+                template.setMessageConverter(new Jackson2JsonMessageConverter());
+                template.convertAndSend("", "game_info", game, m -> {
+                    m.getMessageProperties().setHeader("game_id", game.id);
+                    return m;
+                });
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
